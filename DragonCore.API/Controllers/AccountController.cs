cb304fc33158a3a771902134cfa05e0baef23929
VarunM <VarunM@hollywoodbets.net>
2021-05-18 20:19:38 +0200

    Add project files.

---
+++ DragonCore.API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonCore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetTest")]
+        public IActionResult GetTest()
+        {
+
+            return Ok("Success");
+
+        }
+    }
+}
