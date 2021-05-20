885437654a5559dc7dbb96430ca4077559a3b3f5
VarunM <VarunM@hollywoodbets.net>
2021-05-20 21:58:06 +0200

    feature: ElasticSearch service (WIP) - appended IndexCreation, SingleDocSave, and DocExistCheck

---
+++ ElasticSearch.Infrastructure/Services/Models/ElasticSearchService.cs
@@ -0,0 +1,56 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticSearch.Infrastructure.Services.Models
+{
+    public class ElasticSearchService
+    {
+        public IElasticClient _elasticClient { get; set; }
+
+        public ElasticSearchService(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        private async static void CreateIndex<T>(IElasticClient client, string indexName) where T : class
+        {
+            if (!client.Indices.Exists(indexName).Exists)
+            {
+                var createIndexResponse = await client.Indices.CreateAsync(indexName,
+                    index => index.Map<T>(x => x.AutoMap())
+                );
+            }
+            else
+            {
+                throw new ArgumentException(""); //Placeholder for better handling later
+            }
+        }
+
+        public async Task SaveSingleAsync<T>(T item, string indexname) where T : class
+        {
+            var documentExists = this.CheckDocumentExist<T>(item, indexname);
+
+            if (documentExists.Result)
+            {
+                await _elasticClient.UpdateAsync<T>(item, u => u.Doc(item));
+            }
+            else
+            {
+                await _elasticClient.IndexDocumentAsync(item);
+            }
+        }
+
+        public async Task<Boolean> CheckDocumentExist<T>(T item, string indexname) where T : class
+        {
+            var response =  await _elasticClient.DocumentExistsAsync<T>(item, d => d
+                        .Index(indexname)
+                        );
+
+            return response.Exists;
+        }
+
+    }
+}
